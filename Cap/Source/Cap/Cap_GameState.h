7dc5fa57be96a01fa62803842138c0398c402714
Zhiyi Yao <54916235+AutumnBlaze113@users.noreply.github.com>
2022-03-10 19:31:49 -0500

    Added some small features

---
+++ Cap/Source/Cap/Cap_GameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "Cap_GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAP_API ACap_GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
